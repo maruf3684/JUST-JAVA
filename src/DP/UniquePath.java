da1031cac740346f566be91e8b2204291d6f7482
maruf3684 <maruf3684@gmail.com>
2023-03-17 18:27:31 +0600

    write some dp2

--- src/DP/UniquePath.java
+++ src/DP/UniquePath.java
@@ -4,9 +4,16 @@ import java.util.Arrays;
 
 public class UniquePath {
     public static void main(String[] args) {
-        int[][] dp = new int[101][101];
+        int[][] dp = new int[3][3];
         Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        System.out.println(uniquePath(100, 100, dp));
+
+        //mamoization call
+       // System.out.println(uniquePath(2, 2, dp));
+
+        //tabulation call
+        uniquePathTabulation(dp);
+        System.out.println("Ans->" + Arrays.deepToString(dp));
+        System.out.println(dp[2][2]);
 
     }
 
@@ -31,5 +38,39 @@ public class UniquePath {
 
         return up + left;
     }
+
+    public static int uniquePathTabulation(int[][] dp) {
+
+        //Steps
+        //1. declare base case
+        //2. express all states in for loop
+        //3. copy the recurrence and write
+
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("inner start");
+            for (int j = 0; j < 3; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                }else {
+                    int up = 0;
+                    int left=0;
+                    if (i > 0) {
+                        up = dp[i-1][j];
+                    }
+                    if (j>0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = up + left;
+                    System.out.println(Arrays.deepToString(dp));
+                }
+            }
+            System.out.println("inner end");
+        }
+
+        return 0;
+    }
+
 
 }
